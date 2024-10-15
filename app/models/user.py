47730fedcaac9d5f9f696d514dbf6825d3040d9f
JohanVillard <styx.locke@gmail.com>
2024-10-15 09:00:47 +0200

    Add mail check with email-validator

--- app/models/user.py
+++ app/models/user.py
@@ -1,20 +1,21 @@
 import uuid
 from datetime import datetime
+from email_validator import validate_email, EmailNotValidError
 
 
 class User:
+    """Represent an user."""
+
     def __init__(self, first_name, last_name, email, is_admin=False):
+        """Initialize a User instance."""
         self.is_valid_length(first_name, 1, 50)
-
         self.is_valid_length(last_name, 1, 50)
-
-        if not email:
-            raise ValueError("Email is required.")
+        valid_email = self.check_mail(email)
 
         self.uuid = str(uuid.uuid4())
         self.first_name = first_name
         self.last_name = last_name
-        self.email = email
+        self.email = valid_email
         self.is_admin = is_admin
         self.places = []
         self.created_at = datetime.now()
@@ -29,9 +30,8 @@ class User:
             self.is_valid_length(new_last_name, 1, 50)
             self.last_name = new_last_name
         if new_email is not None:
-            if not new_email:
-                raise ValueError("Email cannot be empty.")
-            self.email = new_email
+            valid_new_email = self.check_mail(new_email)
+            self.email = valid_new_email
         self.updated_at = datetime.now()
 
     def add_place(self, place):
@@ -47,3 +47,12 @@ class User:
         """Check the length of the input."""
         if not (min <= len(input) <= max):
             raise ValueError(f"{input} be between {min} and {max} characters.")
+
+    def check_mail(self, email):
+        """Check the validity of the mail."""
+        try:
+            validated_email = validate_email(email)
+            email = validated_email.email
+        except EmailNotValidError as e:
+            raise ValueError(f"{str(e)}")
+        return email
