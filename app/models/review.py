2b49cb3d53a7e33a60b979e842762ad9d4d6f2eb
JohanVillard <styx.locke@gmail.com>
2024-10-15 21:52:40 +0200

    Small refactoring

--- app/models/review.py
+++ app/models/review.py
@@ -1,9 +1,14 @@
 import uuid
 from datetime import datetime
+from place import Place
+from user import User
 
 
 class Review:
+    """Represent a review."""
+
     def __init__(self, text, rating, place, user):
+        """Initialize an instance of Review class."""
         if not text:
             raise ValueError("Review text is required")
         if not isinstance(rating, int) or not (1 <= rating <= 5):
@@ -22,10 +27,9 @@ class Review:
         self.updated_at = datetime.now()
 
     def update_review(self, text=None, rating=None):
-        """Update review"""
-        if text is not None:
-            if not text:
-                raise ValueError("Review cannot be empty")
+        """Update review."""
+        if text is not None or not text:
+            raise ValueError("Review cannot be empty")
             self.text = text
         if rating is not None:
             if not isinstance(rating, int) or not (1 <= rating <= 5):
@@ -34,4 +38,6 @@ class Review:
         self.updated_at = datetime.now()
 
     def __repr__(self):
-        return f"<Review id={self.id}, rating={self.rating}, place={self.place.title}, user={self.user.first_name} {self.user.last_name}>"
+        return f"<Review id={self.id}, rating={self.rating}, \
+                    place={self.place.title}, \
+                    user={self.user.first_name} {self.user.last_name}>"
