5b3f518021b52294b504f2169864432b44b614d4
JohanVillard <styx.locke@gmail.com>
2024-10-16 14:04:35 +0200

    Add amenities endpoints

--- app/services/facade.py
+++ app/services/facade.py
@@ -1,5 +1,6 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.amenity import Amenity
 
 
 class HBnBFacade:
@@ -16,6 +17,9 @@ class HBnBFacade:
     def __init__(self):
         """Initiliaze a instance of class HBnBFacade."""
         self.user_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
+
+    # ----------------------------------------------------------- #
 
     def create_user(self, user_data):
         """Create an user."""
@@ -44,10 +48,36 @@ class HBnBFacade:
         user.update_user(
             new_first_name=user_data.get("first_name"),
             new_last_name=user_data.get("last_name"),
-            new_email=user_data.get("email")
+            new_email=user_data.get("email"),
         )
 
         # Update the user in the repository
         self.user_repo.update(user_id, user.__dict__)
         return user
 
+    # ----------------------------------------------------------- #
+
+    def create_amenity(self, amenity_data):
+        """Create an amenity."""
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        """Retrieve the amenity's detail."""
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        """Retrieve all amenities."""
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        """Update the amenity."""
+        amenity = self.amenity_repo.get(amenity_id)
+        if amenity is None:
+            return None
+
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return self.amenity_repo.get(amenity_id)
+
+    # ----------------------------------------------------------- #
